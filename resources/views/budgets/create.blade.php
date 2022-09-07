36fcf0874cb81ddb4e8a12378ee27505eb834056
KeigoHashimoto <kei.work.info@gmail.com>
2022-09-07 14:53:43 +0900

    fix

--- resources/views/budgets/create.blade.php
+++ resources/views/budgets/create.blade.php
@@ -5,11 +5,11 @@
     <h2 class="center-title">予算の作成</h2>
         <div class="form-group">
             {{ Form::label('title','予算名') }}
-            {{ Form::text('title',null,['class'=>'form-control']) }}
+            {{ Form::text('title',null,['class'=>'form-control','placeholder'=>'(例)食費、雑費etc']) }}
         </div>
         <div class="form-group">
             {{ Form::label('budget','予算') }}
-            {{ Form::number('budget',null,['class'=>'form-control']) }}
+            {{ Form::number('budget',null,['class'=>'form-control','placeholder'=>'(例)30000円の場合→30000']) }}
         </div>
 
         {{ Form::submit('予算を作成',['class'=>'form-control submit']) }}
