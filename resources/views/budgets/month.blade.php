7f4523b8e421bd2c68b394cd228ff36bacd628f6
KeigoHashimoto <kei.work.info@gmail.com>
2023-01-02 10:34:32 +0900

    fix

--- resources/views/budgets/month.blade.php
+++ resources/views/budgets/month.blade.php
@@ -7,7 +7,7 @@
         <p class="center-title alert">まだ一度も予算が登録されていません</p>
     @else
         @foreach($budgets_month as $budget_month)
-            {!! link_to_route('budget.monthShow',$budget_month->month.'月分',[$budget_month->month],['class'=>'month']) !!}
+            {!! link_to_route('budget.monthShow',$budget_month->month,[$budget_month->month],['class'=>'month']) !!}
         @endforeach
     @endif
 @endsection
