29a2f0f9a499a49ccea62ca15260625eaed570ce
KeigoHashimoto <kei.work.info@gmail.com>
2023-08-13 23:51:17 +0900

    fix

--- resources/views/commons/budgets.blade.php
+++ resources/views/commons/budgets.blade.php
@@ -1,7 +1,5 @@
 
-    @if(empty($budgets))
-        <p class="center-title alert">まだ予算が登録されていません！</p>
-    @else
+    
         @foreach($budgets as $budget)
             <div class="index-budget">
                 <div class="budget-title">
@@ -16,24 +14,21 @@
                 </div>
             </div>  
         @endforeach
-    @endif
 
-    @if($shareBudgets == null)
-    <p class="center-title alert">まだ予算が登録されていません！</p>
-    @else
-        @foreach($shareBudgets as $budget)
-            <div class="index-budget">
-                <div class="budget-title">
-                    <small>{{ $budget->year."年".$budget->month."月" }}  共有された予算 by {{$budget->user->name}}</small>
-                    {!! link_to_route('budget.show','【'.$budget->title.'】',[$budget->id],['class'=>'budget']) !!}
-                </div>
-                <div class="budget-btns">
-                    <a href="{{ route('budget.edit',$budget->id) }}" class="edit"><i class="fas fa-pen"></i></a>
-                    {{ Form::open(['route'=>['budget.delete',$budget->id],'method'=>'delete']) }}
-                        {{ Form::button('<i class="fas fa-trash"></i>',['class'=>'delete','type'=>'submit','onClick'=>'return deleteBtn();']) }}
-                    {{ Form::close() }}
-                </div>
-            </div>  
-        @endforeach
-    @endif
+        @if($shareBudgets != null)
+            @foreach($shareBudgets as $budget)
+                <div class="index-budget">
+                    <div class="budget-title">
+                        <small>{{ $budget->year."年".$budget->month."月" }}  共有された予算 by {{$budget->user->name}}</small>
+                        {!! link_to_route('budget.show','【'.$budget->title.'】',[$budget->id],['class'=>'budget']) !!}
+                    </div>
+                    <div class="budget-btns">
+                        <a href="{{ route('budget.edit',$budget->id) }}" class="edit"><i class="fas fa-pen"></i></a>
+                        {{ Form::open(['route'=>['budget.delete',$budget->id],'method'=>'delete']) }}
+                            {{ Form::button('<i class="fas fa-trash"></i>',['class'=>'delete','type'=>'submit','onClick'=>'return deleteBtn();']) }}
+                        {{ Form::close() }}
+                    </div>
+                </div>  
+            @endforeach
+        @endif
 
